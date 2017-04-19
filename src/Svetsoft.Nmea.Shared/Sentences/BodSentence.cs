a0191817c53b0bfc4394ab80b3013fa9fe3b06a9
Jesus Fernandez <jfevia@gmail.com>
2017-04-19 17:38:28 +0300

    Added support for BOD sentence

---
+++ src/Svetsoft.Nmea.Shared/Sentences/BodSentence.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using Svetsoft.Nmea.Extensions;
+
+namespace Svetsoft.Nmea
+{
+    /// <summary>
+    ///     Represents a sentence of the NMEA specification about Bearing - Waypoint to waypoint.
+    /// </summary>
+    public class BodSentence : NmeaSentence
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="BodSentence" /> class.
+        /// </summary>
+        /// <param name="sentence">The sentence to create the instance from.</param>
+        public BodSentence(string sentence)
+            : base(sentence)
+        {
+            Parse();
+        }
+
+        /// <summary>
+        ///     Returns the true bearing.
+        /// </summary>
+        public Bearing TrueBearing { get; internal set; }
+
+        /// <summary>
+        ///     Returns the magnetic bearing.
+        /// </summary>
+        public Bearing MagneticBearing { get; set; }
+
+        /// <summary>
+        ///     Returns the name of the destination waypoint.
+        /// </summary>
+        public string DestinationWaypointId { get; internal set; }
+
+        /// <summary>
+        ///     Returns the name of the origin waypoint.
+        /// </summary>
+        public string OriginWaypointId { get; internal set; }
+
+        /// <summary>
+        ///     Parses the fields of this sentence to its <see cref="BodSentence" /> equivalent.
+        /// </summary>
+        private void Parse()
+        {
+            var fields = Fields;
+
+            // True bearing
+            if (fields.Length > 1 && fields.All(Enumerable.Range(0, 2), s => !string.IsNullOrWhiteSpace(s)))
+            {
+                TrueBearing = Bearing.Parse(fields.ToArray(0, 2));
+            }
+
+            // Magnetic bearing
+            if (fields.Length > 3 && fields.All(Enumerable.Range(2, 2), s => !string.IsNullOrWhiteSpace(s)))
+            {
+                MagneticBearing = Bearing.Parse(fields.ToArray(2, 2));
+            }
+
+            // Destination waypoint
+            if (fields.Length > 4 && !string.IsNullOrWhiteSpace(fields[4]))
+            {
+                DestinationWaypointId = fields[4];
+            }
+
+            // Origin waypoint
+            if (fields.Length > 5 && !string.IsNullOrWhiteSpace(fields[5]))
+            {
+                OriginWaypointId = fields[5];
+            }
+        }
+    }
+}
---
+++ src/Svetsoft.Nmea.Tests/BodSentenceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svetsoft.Nmea.Tests
+{
+    [TestClass]
+    public class BodSentenceTests
+    {
+        /// <summary>
+        ///     Checks whether a parsed BOD sentence from the NMEA specification equals the string it was built from.
+        /// </summary>
+        [TestMethod]
+        public void ParseBodSentence()
+        {
+            var sentence = new BodSentence("$GPBOD,011,T,011,M,DEST,ORIG*52");
+
+            // NmeaSentence (Inherited)
+            Assert.AreEqual("$GPBOD,011,T,011,M,DEST,ORIG*52", sentence.Sentence);
+            Assert.AreEqual("GPBOD", sentence.MessageType);
+            Assert.AreEqual("52", sentence.Checksum);
+            CollectionAssert.AreEqual(new[]
+            {
+                "011",
+                "T",
+                "011",
+                "M",
+                "DEST",
+                "ORIG"
+            }, sentence.Fields);
+
+            // BOD-specific
+            Assert.AreEqual(011, sentence.TrueBearing.Value.Sexagesimal.Degrees);
+            Assert.AreEqual(AbsoluteBearing.True, sentence.TrueBearing.AbsoluteBearing);
+            Assert.AreEqual(011, sentence.MagneticBearing.Value.Sexagesimal.Degrees);
+            Assert.AreEqual(AbsoluteBearing.Magnetic, sentence.MagneticBearing.AbsoluteBearing);
+            Assert.AreEqual("DEST", sentence.DestinationWaypointId);
+            Assert.AreEqual("ORIG", sentence.OriginWaypointId);
+        }
+    }
+}
