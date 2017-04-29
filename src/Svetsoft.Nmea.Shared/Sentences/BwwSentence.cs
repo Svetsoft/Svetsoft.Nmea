5a2b0e56ee88e0e83d1eb0c7d5821f8c0935d3ed
Jesus Fernandez <jfevia@gmail.com>
2017-04-29 23:25:48 +0300

    Added support for BWW sentence

---
+++ src/Svetsoft.Nmea.Shared/Sentences/BwwSentence.cs
@@ -0,0 +1,49 @@
+﻿namespace Svetsoft.Nmea
+{
+    /// <summary>
+    ///     Represents a sentence of the NMEA specification for bearing (Waypoint to waypoint).
+    /// </summary>
+    public class BwwSentence : NmeaSentence, ITrueBearingSentence, IMagneticBearingSentence
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="BwwSentence" /> class.
+        /// </summary>
+        /// <param name="sentence">The sentence to create the instance from.</param>
+        public BwwSentence(string sentence)
+            : base(sentence)
+        {
+            Parse();
+        }
+
+        /// <summary>
+        ///     Returns the true bearing.
+        /// </summary>
+        public Bearing TrueBearing { get; internal set; }
+
+        /// <summary>
+        ///     Returns the magnetic bearing.
+        /// </summary>
+        public Bearing MagneticBearing { get; set; }
+
+        /// <summary>
+        ///     Returns the destination waypoint.
+        /// </summary>
+        public Waypoint DestinationWaypoint { get; internal set; }
+
+        /// <summary>
+        ///     Returns the origin waypoint.
+        /// </summary>
+        public Waypoint OriginWaypoint { get; internal set; }
+
+        /// <summary>
+        ///     Parses the fields of this sentence to its <see cref="BwwSentence" /> equivalent.
+        /// </summary>
+        private void Parse()
+        {
+            TrueBearing = GetBearing(0);
+            MagneticBearing = GetBearing(2);
+            DestinationWaypoint = GetWaypoint(4);
+            OriginWaypoint = GetWaypoint(5);
+        }
+    }
+}
---
+++ src/Svetsoft.Nmea.Tests/BwwSentenceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svetsoft.Nmea.Tests
+{
+    [TestClass]
+    public class BwwSentenceTests
+    {
+        /// <summary>
+        ///     Checks whether a parsed BWW sentence from the NMEA specification equals the string it was built from.
+        /// </summary>
+        [TestMethod]
+        public void ParseBwwSentence()
+        {
+            var sentence = new BwwSentence("$GPBWW,011,T,011,M,DEST,ORIG*59");
+
+            // NmeaSentence (Inherited)
+            Assert.AreEqual("$GPBWW,011,T,011,M,DEST,ORIG*59", sentence.Sentence);
+            Assert.AreEqual("GPBWW", sentence.MessageType);
+            Assert.AreEqual("59", sentence.Checksum);
+            CollectionAssert.AreEqual(new[]
+            {
+                "011",
+                "T",
+                "011",
+                "M",
+                "DEST",
+                "ORIG"
+            }, sentence.Fields);
+
+            // BWW-specific
+            Assert.AreEqual(011, sentence.TrueBearing.Value.Sexagesimal.Degrees);
+            Assert.AreEqual(AbsoluteBearing.True, sentence.TrueBearing.AbsoluteBearing);
+            Assert.AreEqual(011, sentence.MagneticBearing.Value.Sexagesimal.Degrees);
+            Assert.AreEqual(AbsoluteBearing.Magnetic, sentence.MagneticBearing.AbsoluteBearing);
+            Assert.AreEqual("DEST", sentence.DestinationWaypoint.Name);
+            Assert.AreEqual("ORIG", sentence.OriginWaypoint.Name);
+        }
+    }
+}
