c536b3bea92b62d0b080b3d0b17512b06c23fdc7
Jesus Fernandez <jfevia@gmail.com>
2017-04-29 21:11:20 +0300

    Added support for BWR sentence

---
+++ src/Svetsoft.Nmea.Shared/Sentences/BwrSentence.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Svetsoft.Nmea
+{
+    /// <summary>
+    ///     Represents a sentence of the NMEA specification for bearing and distance to waypoint (Rhumb line).
+    /// </summary>
+    public class BwrSentence : NmeaSentence, ITrueBearingSentence, IMagneticBearingSentence
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="BwrSentence" /> class.
+        /// </summary>
+        /// <param name="sentence">The sentence to create the instance from.</param>
+        public BwrSentence(string sentence)
+            : base(sentence)
+        {
+            Parse();
+        }
+
+        /// <summary>
+        ///     Returns the true bearing.
+        /// </summary>
+        public Bearing TrueBearing { get; internal set; }
+
+        /// <summary>
+        ///     Returns the magnetic bearing.
+        /// </summary>
+        public Bearing MagneticBearing { get; set; }
+
+        /// <summary>
+        ///     Returns the waypoint.
+        /// </summary>
+        public Waypoint Waypoint { get; internal set; }
+
+        /// <summary>
+        ///     Returns the position of the waypoint.
+        /// </summary>
+        public Position WaypointPosition { get; internal set; }
+
+        /// <summary>
+        ///     Returns the time of day, expressed as the Coordinated Universal Time (UTC).
+        /// </summary>
+        public TimeSpan UtcTime { get; internal set; }
+
+        /// <summary>
+        ///     Returns the <see cref="Distance" /> to the waypoint.
+        /// </summary>
+        public Distance WaypointDistance { get; internal set; }
+
+        /// <summary>
+        ///     Parses the fields of this sentence to its <see cref="BwrSentence" /> equivalent.
+        /// </summary>
+        private void Parse()
+        {
+            UtcTime = GetUtcTime(0);
+            WaypointPosition = GetPosition(1);
+            TrueBearing = GetBearing(5);
+            MagneticBearing = GetBearing(7);
+            WaypointDistance = GetDistance(9);
+            Waypoint = GetWaypoint(11);
+        }
+    }
+}
---
+++ src/Svetsoft.Nmea.Tests/BwrSentenceTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svetsoft.Nmea.Tests
+{
+    [TestClass]
+    public class BwrSentenceTests
+    {
+        /// <summary>
+        ///     Checks whether a parsed BWR sentence from the NMEA specification equals the string it was built from.
+        /// </summary>
+        [TestMethod]
+        public void ParseBwrSentence()
+        {
+            var sentence = new BwrSentence("$GPBWR,220516,5130.02,N,00046.34,W,213.8,T,218.0,M,0004.6,N,EGLM*30");
+
+            // NmeaSentence (Inherited)
+            Assert.AreEqual("$GPBWR,220516,5130.02,N,00046.34,W,213.8,T,218.0,M,0004.6,N,EGLM*30", sentence.Sentence);
+            Assert.AreEqual("GPBWR", sentence.MessageType);
+            Assert.AreEqual("30", sentence.Checksum);
+            CollectionAssert.AreEqual(new[]
+            {
+                "220516",
+                "5130.02",
+                "N",
+                "00046.34",
+                "W",
+                "213.8",
+                "T",
+                "218.0",
+                "M",
+                "0004.6",
+                "N",
+                "EGLM"
+            }, sentence.Fields);
+
+            // BWR-specific
+            Assert.AreEqual(new TimeSpan(22, 05, 16), sentence.UtcTime);
+            Assert.AreEqual(5130.02, sentence.WaypointPosition.Latitude.Sexagesimal.Degrees);
+            Assert.AreEqual(LatitudeHemisphere.North, sentence.WaypointPosition.Latitude.Hemisphere);
+            Assert.AreEqual(00046.34, sentence.WaypointPosition.Longitude.Sexagesimal.Degrees);
+            Assert.AreEqual(LongitudeHemisphere.West, sentence.WaypointPosition.Longitude.Hemisphere);
+            Assert.AreEqual(213.8, sentence.TrueBearing.Value.Sexagesimal.Degrees);
+            Assert.AreEqual(AbsoluteBearing.True, sentence.TrueBearing.AbsoluteBearing);
+            Assert.AreEqual(218.0, sentence.MagneticBearing.Value.Sexagesimal.Degrees);
+            Assert.AreEqual(AbsoluteBearing.Magnetic, sentence.MagneticBearing.AbsoluteBearing);
+            Assert.AreEqual(0004.6, sentence.WaypointDistance.Value);
+            Assert.AreEqual(DistanceUnit.NauticalMiles, sentence.WaypointDistance.Unit);
+            Assert.AreEqual("EGLM", sentence.Waypoint.Name);
+        }
+    }
+}
