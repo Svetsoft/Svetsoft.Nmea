575094b630ef9681dbb61f94bca9191eefd8d94e
Jesus Fernandez <jfevia@gmail.com>
2017-04-30 03:49:41 +0300

    Added support for DCN sentence (Obsolete) and remove unit tests for obsolete sentences

---
+++ src/Svetsoft.Nmea.Shared/Sentences/DcnSentence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Svetsoft.Nmea
+{
+    /// <summary>
+    ///     Represents a sentence of the NMEA specification for Decca Position.
+    /// </summary>
+    public sealed class DcnSentence : NmeaSentence
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="DcnSentence" /> class.
+        /// </summary>
+        /// <param name="sentence">The sentence to create the instance from.</param>
+        [Obsolete("Decca Position sentence (DCN) has been designated obsolete by NMEA as of 3.01.")]
+        public DcnSentence(string sentence)
+            : base(sentence)
+        {
+            throw new NotImplementedException($"{nameof(DcnSentence)} is not implemented");
+        }
+    }
+}
