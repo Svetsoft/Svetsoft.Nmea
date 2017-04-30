1ee059ed5b8435a08d538d0e9b008625147b294e
Jesus Fernandez <jfevia@gmail.com>
2017-04-30 03:56:58 +0300

    Added support for DPT sentence

---
+++ src/Svetsoft.Nmea.Shared/Sentences/DptSentence.cs
@@ -0,0 +1,38 @@
+﻿namespace Svetsoft.Nmea
+{
+    /// <summary>
+    ///     Represents a sentence of the NMEA specification for depth of water.
+    /// </summary>
+    public class DptSentence : NmeaSentence
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="DptSentence" /> class.
+        /// </summary>
+        /// <param name="sentence">The sentence to create the instance from.</param>
+        public DptSentence(string sentence)
+            : base(sentence)
+        {
+            Parse();
+        }
+
+        /// <summary>
+        ///     Returns the depth in meters.
+        /// </summary>
+        public Distance Depth { get; internal set; }
+
+        /// <summary>
+        ///     Returns the offset from transducer. When this value is positive it means the distance is measured from tansducer to
+        ///     water line. Otherwise, it means the distance is measured from transducer to keel.
+        /// </summary>
+        public double TransducerOffset { get; internal set; }
+
+        /// <summary>
+        ///     Parses the fields of this sentence to its <see cref="DptSentence" /> equivalent.
+        /// </summary>
+        private void Parse()
+        {
+            Depth = GetDistance(0);
+            TransducerOffset = GetDouble(2);
+        }
+    }
+}
---
+++ src/Svetsoft.Nmea.Tests/DptSentenceTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svetsoft.Nmea.Tests
+{
+    [TestClass]
+    public class DptSentenceTests
+    {
+        /// <summary>
+        ///     Checks whether a parsed DPT sentence from the NMEA specification equals the string it was built from.
+        /// </summary>
+        [TestMethod]
+        public void ParseDptSentence()
+        {
+            var sentence = new DptSentence("$GPDPT,20.0,M,-5.0*2C");
+
+            // NmeaSentence (Inherited)
+            Assert.AreEqual("$GPDPT,20.0,M,-5.0*2C", sentence.Sentence);
+            Assert.AreEqual("GPDPT", sentence.MessageType);
+            Assert.AreEqual("2C", sentence.Checksum);
+            CollectionAssert.AreEqual(new[]
+            {
+                "20.0",
+                "M",
+                "-5.0"
+            }, sentence.Fields);
+
+            // DPT-specific
+            Assert.AreEqual(20.0, sentence.Depth.Value);
+            Assert.AreEqual(DistanceUnit.Meters, sentence.Depth.Unit);
+            Assert.AreEqual(-5.0, sentence.TransducerOffset);
+        }
+    }
+}
