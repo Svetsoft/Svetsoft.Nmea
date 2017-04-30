753d9ef40f4bbcf55b793c472a69fee4b7b2a1d5
Jesus Fernandez <jfevia@gmail.com>
2017-04-30 03:05:14 +0300

    Added support for DBT sentence

---
+++ src/Svetsoft.Nmea.Shared/Sentences/DbtSentence.cs
@@ -0,0 +1,43 @@
+﻿namespace Svetsoft.Nmea
+{
+    /// <summary>
+    ///     Represents a sentence of the NMEA specification for depth below transducer.
+    /// </summary>
+    public class DbtSentence : NmeaSentence
+    {
+        /// <summary>
+        ///     Creates a new instance of the <see cref="DbtSentence" /> class.
+        /// </summary>
+        /// <param name="sentence">The sentence to create the instance from.</param>
+        public DbtSentence(string sentence)
+            : base(sentence)
+        {
+            Parse();
+        }
+
+        /// <summary>
+        ///     Returns the depth in feet.
+        /// </summary>
+        public Distance FeetDepth { get; internal set; }
+
+        /// <summary>
+        ///     Returns the depth in meters.
+        /// </summary>
+        public Distance MetersDepth { get; internal set; }
+
+        /// <summary>
+        ///     Returns the depth in fathoms.
+        /// </summary>
+        public Distance FathomsDepth { get; internal set; }
+
+        /// <summary>
+        ///     Parses the fields of this sentence to its <see cref="DbtSentence" /> equivalent.
+        /// </summary>
+        private void Parse()
+        {
+            FeetDepth = GetDistance(0);
+            MetersDepth = GetDistance(2);
+            FathomsDepth = GetDistance(4);
+        }
+    }
+}
---
+++ src/Svetsoft.Nmea.Tests/DbtSentenceTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Svetsoft.Nmea.Tests
+{
+    [TestClass]
+    public class DbtSentenceTests
+    {
+        /// <summary>
+        ///     Checks whether a parsed DBT sentence from the NMEA specification equals the string it was built from.
+        /// </summary>
+        [TestMethod]
+        public void ParseDbtSentence()
+        {
+            var sentence = new DbtSentence("$GPDBT,20.0,f,20.0,M,20.0,F*34");
+
+            // NmeaSentence (Inherited)
+            Assert.AreEqual("$GPDBT,20.0,f,20.0,M,20.0,F*34", sentence.Sentence);
+            Assert.AreEqual("GPDBT", sentence.MessageType);
+            Assert.AreEqual("34", sentence.Checksum);
+            CollectionAssert.AreEqual(new[]
+            {
+                "20.0",
+                "f",
+                "20.0",
+                "M",
+                "20.0",
+                "F"
+            }, sentence.Fields);
+
+            // DBT-specific
+            Assert.AreEqual(20.0, sentence.FeetDepth.Value);
+            Assert.AreEqual(DistanceUnit.Feet, sentence.FeetDepth.Unit);
+            Assert.AreEqual(20.0, sentence.MetersDepth.Value);
+            Assert.AreEqual(DistanceUnit.Meters, sentence.MetersDepth.Unit);
+            Assert.AreEqual(20.0, sentence.FathomsDepth.Value);
+            Assert.AreEqual(DistanceUnit.Fathoms, sentence.FathomsDepth.Unit);
+        }
+    }
+}
